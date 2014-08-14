cbba725e349afb7ed9a44fd569ae4523d4242b61
hagridpan <apanbo@qq.com>
2014-08-14 22:16:52 +0800

    20140814-2216

---
+++ src/main/java/org/pbc/qzkg/firstQuick/entity/IdEntity.java
@@ -0,0 +1,23 @@
+/**
+* <p>Title: IdEntity.java</p>
+* <p>Description: </p>
+* <p>Copyright: Copyright (c) 2014</p>
+* <p>Company: pbc_qzgk</p>
+* @author Hagrid Pan
+* @date 2014年8月14日
+* @version 1.0
+*/
+package org.pbc.qzkg.firstQuick.entity;
+
+
+
+/**
+ * <p>Title: IdEntity</p>
+ * <p>Description: </p>
+ * <p>Company: pbc_qzgk</p> 
+ * @author   Hagrid Pan
+ * @date       2014年8月14日
+ */
+public class IdEntity {
+
+}
